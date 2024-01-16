7dcaa5e031d48fa52e6d10508893b4fc64915d50
Adam <adamwmcneil@gmail.com>
2024-01-16 10:28:13 -0600

    added summary page

--- client-svelte/src/lib/menus/Reveal.svelte
+++ client-svelte/src/lib/menus/Reveal.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/Button.svelte';
 	import { onMount } from 'svelte';
-	import { getGame, } from '$lib/functions/requests';
+	import { getGame } from '$lib/functions/requests';
 	import { sleep } from '$lib/functions/helper';
 
 	export let setGameState: (new_state: string) => void;
@@ -11,6 +11,8 @@
 	let player_two: String;
 	let player_one_question: String;
 	let player_two_question: String;
+	let player_one_answer: String;
+	let player_two_answer: String;
 	let round_count: number;
 
 	async function readGame() {
@@ -19,8 +21,10 @@
 			.then((data) => {
 				player_one = data.player_one;
 				player_two = data.player_two;
-				player_one_question = data.rounds[data.rounds.length - 1].player_one_question;
-				player_two_question = data.rounds[data.rounds.length - 1].player_two_question;
+				player_one_question = data.rounds[data.rounds.length - 2].player_one_question;
+				player_two_question = data.rounds[data.rounds.length - 2].player_two_question;
+				player_one_answer = data.rounds[data.rounds.length - 2].player_one_answer;
+				player_two_question = data.rounds[data.rounds.length - 2].player_two_question;
 				round_count = data.rounds.length;
 			});
 	}
@@ -41,23 +45,46 @@
 
 <main>
 	<h2>
-		Round: {round_count}
+		Round {round_count - 1} Summary
 	</h2>
-	<h3>
-		Click the button to get the next question and start a timer
-		<h3>
-			<div>
-				<Button
-					text={'reveal next question'}
-					onClick={() => {
-						setGameState('answer');
-					}}
-				/>
-			</div>
-		</h3>
-	</h3>
+	<div class = "flex">
+	<div>
+		<div class="orange">
+			Team {player_one}
+		</div>
+		<div>
+			{player_one_question}: {player_one_answer}
+		</div>
+	</div>
+	<div>
+		<div class="purple">
+			Team {player_two}
+		</div>
+		<div>
+			{player_two_question}: {player_two_answer}
+		</div>
+	</div>
+	</div>
+	<div>
+		<Button
+			text={'Next Round'}
+			onClick={() => {
+				setGameState('answer');
+			}}
+		/>
+	</div>
 </main>
 
 <style>
 	@import '../../app.css';
+	.flex {
+		/* display: flex; */
+		text-align: center;
+	}
+	.purple {
+		color: purple;
+	}
+	.orange {
+		color: orange;
+	}
 </style>
